fa3c6ff240627182a003a0e6eac6b7f661d16ecc
Elin Blomgren <elinblomgren@gmail.com>
2020-09-21 17:53:32 +0200

    months in dropdown and minor refactor

--- app/src/main/java/com/helgegren/wallpapercalender/ChangeTrigger.java
+++ app/src/main/java/com/helgegren/wallpapercalender/ChangeTrigger.java
@@ -1,17 +1,46 @@
 package com.helgegren.wallpapercalender;
 
+import android.app.job.JobInfo;
 import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
 import android.app.job.JobService;
+import android.content.ComponentName;
+import android.content.Context;
 import android.util.Log;
 
-import java.util.Date;
+import java.util.Calendar;
 
 public class ChangeTrigger extends JobService {
 
+    private static final String LOG_TAG = "ELIN";
+
+    static void triggerJob(Context context) {
+
+        JobScheduler jobScheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        if (null == jobScheduler) {
+            Log.w(LOG_TAG, "Could not trigger job, no jobscheduler availible from context");
+            return;
+        }
+        Calendar cal = Calendar.getInstance();
+        long currentMs = cal.getTimeInMillis();
+        //cal.set(cal.get(Calendar.YEAR), currentMonth + 1 % 12, 1);
+        //cal.set(Calendar.DAY_OF_MONTH, 1);
+        cal.set(Calendar.SECOND, cal.get(Calendar.SECOND) + 10);
+        long timeToTrigger = cal.getTimeInMillis() - currentMs;
+        Log.v(LOG_TAG, cal.getTime() + " date");
+        Log.v(LOG_TAG, timeToTrigger + " ms");
+
+        JobInfo jobInfo = new JobInfo.Builder(5, new ComponentName(context, ChangeTrigger.class))
+                .setMinimumLatency(timeToTrigger)
+                .build();
+
+        jobScheduler.schedule(jobInfo);
+    }
+
     @Override
     public boolean onStartJob(JobParameters params) {
-        Log.v("ELIN", "triggered!");
-        int month = new Date().getMonth();
+        int month = Calendar.getInstance().get(Calendar.MONTH);
+        Log.v("ELIN", "triggered!" + month);
         new WallpaperChanger(getApplicationContext()).setBackground(month);
         return false;
     }
--- app/src/main/java/com/helgegren/wallpapercalender/MainActivity.java
+++ app/src/main/java/com/helgegren/wallpapercalender/MainActivity.java
@@ -1,11 +1,6 @@
 package com.helgegren.wallpapercalender;
 
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -29,25 +24,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mLayout = findViewById(R.id.spinner_view);
-
         mWallpaperChanger = new WallpaperChanger(getApplicationContext());
 
         setUpButtons();
-        triggerJob();
+        ChangeTrigger.triggerJob(getApplicationContext());
     }
 
     private void setUpButtons() {
         Calendar calendar;
+        String[] arraySpinner = new String[Calendar.UNDECIMBER];
         for (int i = 0; i <= Calendar.DECEMBER; i++) {
-            Button button = new Button(this);
             calendar = Calendar.getInstance();
+            calendar.set(Calendar.MONTH, i);
             String month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
-            button.setText(month);
+            arraySpinner[i] = month;
         }
 
-        String[] arraySpinner = new String[]{
-                "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"
-        };
         mSpinner = new Spinner(this);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, arraySpinner);
@@ -66,26 +58,5 @@ public class MainActivity extends AppCompatActivity {
         mWallpaperChanger.setBackground(selectedMonth);
     }
 
-    private void triggerJob() {
-
-        JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
-        if (null == jobScheduler) {
-            return;
-        }
-        Calendar cal = Calendar.getInstance();
-        long currentMs = cal.getTimeInMillis();
-        //cal.set(cal.get(Calendar.YEAR), currentMonth + 1 % 12, cal.get(Calendar.DATE));
-        //cal.set(Calendar.DAY_OF_MONTH, 1);
-        cal.set(Calendar.SECOND, cal.get(Calendar.SECOND) + 10);
-        long timeToTrigger = cal.getTimeInMillis() - currentMs;
-        Log.v("ELIN", cal.getTime() + " date");
-        Log.v("ELIN", timeToTrigger + " ms");
-
-        JobInfo jobInfo = new JobInfo.Builder(5, new ComponentName(getApplicationContext(), ChangeTrigger.class))
-                .setMinimumLatency(timeToTrigger)
-                .build();
-
-        jobScheduler.schedule(jobInfo);
-    }
 }
 
